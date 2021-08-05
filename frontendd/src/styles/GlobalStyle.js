f7c77822ea10a20e149fbe0413d14466a1a647e0
Jean Grijp <grijpjean@gmail.com>
2021-08-04 23:31:25 -0300

    mudando o GlobalStyle

--- frontendd/src/styles/GlobalStyle.js
+++ frontendd/src/styles/GlobalStyle.js
@@ -7,10 +7,20 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-size: 15px;
   }
   
+  html {
+     scroll-behavior: smooth;
+     font-size: 62.5%
+  }
+
   body {
-    background: ${props => props.theme.colours.background};
+    background: ${props => props.theme.colours.backgroundBorder};
+  }
+
+  .container {
+    max-width: 1440px;
+    margin: 0 auto;
+    width: 90vw;
   }
 `;
