8b4749c793ff7915ef7c3f7e43541416bda99fc8
Jean Grijp <grijpjean@gmail.com>
2021-08-25 18:34:47 -0300

    .

--- frontendd/src/components/Home/Home.jsx
+++ frontendd/src/components/Home/Home.jsx
@@ -1,9 +1,17 @@
 
 import { FirstPage } from "../FirstPage/FirstPage"
+import { SecondPage } from "../SecondPage/SecondPage"
+import { SignIn } from "../SignIn/SignIn"
+import { SignUp } from "../SignUp/SignUp"
 
 
 export const Home = () => {
   return (
+    <>
       <FirstPage/>
+      <SecondPage/>
+      {/* <SignIn/> */}
+      {/* <SignUp/> */}
+    </>
   )
 }
